a0e8979d14295d488be1679c7aded39e7c31fee5
CaptainDeer <erikhernandez323@gmail.com>
2021-07-24 13:40:13 -0500

    Adding navigation and all the views

---
+++ app/src/main/java/com/captaindeer/rookmotionchallengemvvm/ui/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.captaindeer.rookmotionchallengemvvm.ui
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import com.captaindeer.rookmotionchallengemvvm.R
+import com.captaindeer.rookmotionchallengemvvm.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toggle: ActionBarDrawerToggle
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //NavBar
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+                R.id.extra -> Toast.makeText(this, "Espacio extra", Toast.LENGTH_SHORT).show()
+                R.id.profile -> Toast.makeText(this, "Configuracion", Toast.LENGTH_SHORT).show()
+                R.id.logout -> Toast.makeText(this, "Espacio extra", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+    }
+
+    override fun onBackPressed() {
+        if (this.binding.drawerLayout.isDrawerOpen(GravityCompat.START))
+            this.binding.drawerLayout.closeDrawer(GravityCompat.START)
+        else
+            super.onBackPressed()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item))
+            return true
+        return super.onOptionsItemSelected(item)
+    }
+}
