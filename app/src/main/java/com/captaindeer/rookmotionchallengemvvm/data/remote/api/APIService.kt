2caa480e437e177b64348d804e270e3dcd84ad45
CaptainDeer <erikhernandez323@gmail.com>
2021-07-28 12:35:54 -0500

    feature/mvvm

--- app/src/main/java/com/captaindeer/rookmotionchallengemvvm/data/remote/api/APIService.kt
+++ app/src/main/java/com/captaindeer/rookmotionchallengemvvm/data/remote/api/APIService.kt
@@ -1,6 +1,8 @@
 package com.captaindeer.rookmotionchallengemvvm.data.remote.api
 
+import com.captaindeer.rookmotionchallengemvvm.data.remote.model.DataResponse
 import com.captaindeer.rookmotionchallengemvvm.data.remote.model.UsersResponse
+import io.reactivex.Single
 import retrofit2.Response
 import retrofit2.http.GET
 
